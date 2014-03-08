9b072abb6857877c3ebafbd90aedebd0b4dba4d0
Marek <mbednarczyk@live.com>
2014-03-08 14:54:05 +0100

    Zmiany zmiany i zmiany

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -28,7 +28,7 @@ class GistsController < ApplicationController
 
     respond_to do |format|
       if @gist.save
-        format.html { redirect_to @gist, notice: 'Gist was successfully created.' }
+        format.html { redirect_to @gist, notice: 'Wrzutka została dodana!' }
         format.json { render action: 'show', status: :created, location: @gist }
       else
         format.html { render action: 'new' }
@@ -42,7 +42,7 @@ class GistsController < ApplicationController
   def update
     respond_to do |format|
       if @gist.update(gist_params)
-        format.html { redirect_to @gist, notice: 'Gist was successfully updated.' }
+        format.html { redirect_to @gist, notice: 'Wrzutka została zaktualizowana!' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
