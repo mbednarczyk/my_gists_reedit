1883f7f08624ab7e058b06d090a4133af76d2b2c
Marek <mbednarczyk@live.com>
2014-03-29 15:10:31 +0100

    fixing

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -8,7 +8,7 @@ class GistsController < ApplicationController
   end
 
   def get_and_show_gists
-    @gists = Gist.paginate(page: params[:page], per_page: 9).order('created_at DESC')
+    @gists = Gist.paginate(page: params[:page], per_page: 12).order('created_at DESC')
     respond_to do |format|
       format.html
       format.js
